29f769c9698ffe6d4b67244b172ee2c8ae81cf6b
brahim <brahhim.mess@gmail.com>
2018-05-09 00:49:50 +0200

    ddd

--- src/app/input-format.directive.ts
+++ src/app/input-format.directive.ts
@@ -1,23 +1,10 @@
-import {Directive, ElementRef, HostListener, Input} from '@angular/core';
+import { Directive } from '@angular/core';
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
 
-  @Input('appInputFormat') format;
-    constructor(private el: ElementRef) { }
-
-  @HostListener('blur') onBlur() {
-    let value:string  = this.el.nativeElement.value;
-
-
-    if (this.format == 'lowercase')
-        this.el.nativeElement.value = value.toLowerCase();
-    else
-        this.el.nativeElement.value = value.toUpperCase();
-
-  }
-
+  constructor() { }
 
 }
